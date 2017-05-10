5ed27c62cc5ed421bd4e9dc42e54d8ed8c02f632
Oliver Lorenz <mail@oliverlorenz.com>
2017-05-10 22:22:10 +0200

    added 'get list' and 'get list cards'

---
+++ trello/84_trello-get-list-cards.js
@@ -0,0 +1,24 @@
+var Trello = require('node-trello')
+
+module.exports = function (RED) {
+  function TrelloGetListNode (config) {
+    RED.nodes.createNode(this, config)
+    var node = this
+
+    // Retrieve the config node
+    var credentialNode = RED.nodes.getNode(config.trello)
+    var trello = new Trello(credentialNode.apikey, credentialNode.secret)
+    this.on('input', function (msg) {
+      var trelloData = msg.trello || {}
+      var listId = trelloData.idList || config.idList
+      trello.get(
+        '/1/lists/' + listId + '/cards',
+        (err, data) => {
+          if (err) { node.error(err) }
+          node.send({ payload: data })
+        }
+      )
+    })
+  }
+  RED.nodes.registerType('trello-get-list-cards', TrelloGetListNode)
+}
